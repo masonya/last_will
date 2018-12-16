932d9939a28077438654a724f0d8f7643710088f
masonya <31894058+masonya@users.noreply.github.com>
2018-12-16 10:39:17 +0300

    Стили Лолиты

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_130040) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 2018_12_09_104834) do
 
   create_table "account_transfers", force: :cascade do |t|
     t.integer "transmitter_id"
@@ -48,7 +45,6 @@ ActiveRecord::Schema.define(version: 2018_12_11_130040) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "login"
   end
 
   create_table "subscriptions", force: :cascade do |t|
